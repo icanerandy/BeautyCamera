36dd58fe67b6d3a6825fc99ad9e47c384e271ead
icaner <icanerandy@gmail.com>
2023-05-30 00:24:52 +0800

    完成了人脸面部（人脸及五官）检测

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import base64
 import json
-
-import cv2
-import numpy as np
-from PyQt5 import uic
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPixmap, QImage
 import sys
 
-import GUI
+import cv2
+import dlib
+import numpy as np
+
+from PyQt5 import uic
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import *
+
 from Camera import Camera
 
 
@@ -34,7 +35,6 @@ class MainWindow(QMainWindow):
         # 子线程实例
         self.camera_thread = Camera(self.open_cam_complete_signal)
 
-
     def init_ui(self):
         # 加载由Qt Designer设计的ui文件
         self.ui = uic.loadUi('./GUI.ui', self)
@@ -55,8 +55,8 @@ class MainWindow(QMainWindow):
         # 弹出来的显示图片的窗口会随着图片尺寸大小的变化而变化
         img_name, _ = QFileDialog.getOpenFileName(None, '打开文件', '.', '图片文件 (*.png, *.jpg)')
         # 得到图片文件名
-        self.image = img_name
-        self.res_image = img_name
+        self.image = cv2.imread(img_name)
+        self.face_detect()
         self.show_image()
 
     def save_image(self):
@@ -64,7 +64,10 @@ class MainWindow(QMainWindow):
         cv2.imwrite(img_name, self.res_image)
 
     def show_image(self):
-        self.ui.label.setPixmap(QPixmap(self.image))
+        img = self.image
+        frame = QImage(img, img.shape[1], img.shape[0], img.strides[0], QImage.Format_RGB888).rgbSwapped()
+        pix = QPixmap.fromImage(frame)
+        self.ui.label.setPixmap(QPixmap(pix))
 
     def open_camera(self):
         # 执行线程的run方法
@@ -72,7 +75,8 @@ class MainWindow(QMainWindow):
 
     def view_camera(self, img_json):
         self.img_json = img_json
-        img = self.decoder(self.img_json)
+        self.image = self.decoder(self.img_json)
+        img = self.image
         frame = QImage(img, img.shape[1], img.shape[0], img.strides[0], QImage.Format_RGB888).rgbSwapped()
         pix = QPixmap.fromImage(frame)
         self.ui.label.setPixmap(QPixmap(pix))
@@ -91,6 +95,26 @@ class MainWindow(QMainWindow):
         jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
         img = cv2.imdecode(jpg_as_np, flags=1)
         return img
+
+    def face_detect(self):
+        # 将图像转为灰度图像
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGRA2GRAY)
+
+        # 人脸检测分类器
+        detector = dlib.get_frontal_face_detector()
+        # 获取人脸检测器
+        predictor = dlib.shape_predictor(
+            './trainner/shape_predictor_68_face_landmarks.dat'
+        )
+
+        dets = detector(gray, 1)
+        for face in dets:
+            # 寻找人脸的68个标定点
+            shape = predictor(self.image, face)
+            # 遍历所有点，打印出其坐标，并圈出来
+            for pt in shape.parts():
+                pt_pos = (pt.x, pt.y)
+                cv2.circle(self.image, pt_pos, 2, (0, 255, 0), 1)
 
 
 if __name__ == "__main__":
