6569aeb591da151feef33fa0fbfba561eb072bcd
unknown <2359986724@qq.com>
2023-05-30 17:00:43 +0800

    完成了图像和实时美白功能

--- main.py
+++ main.py
@@ -15,9 +15,18 @@ from Camera import Camera
 from ProcessUI import ProcessWindow
 
 
+jaw_point = list(range(0, 17)) + list(range(68,81))
+left_eye = list(range(42, 48))
+right_eye = list(range(36, 42))
+left_brow = list(range(22, 27))
+right_brow = list(range(17, 22))
+mouth = list(range(48, 61))
+nose = list(range(27, 35))
+
+
 class MainWindow(QMainWindow):
     # 子线程返回摄像头图像到主线程的信号
-    open_cam_complete_signal = pyqtSignal(str)
+    open_cam_complete_signal = pyqtSignal(str, np.matrix)
     slider_change_signal = pyqtSignal(int, int)
 
     def __init__(self):
@@ -28,6 +37,11 @@ class MainWindow(QMainWindow):
         self.image = None
         self.img_json = None
         self.res_image = None
+
+        # 人脸坐标矩阵
+        self.land_mask = None
+        # 皮肤坐标矩阵
+        self.skin_mask = None
 
         # 滑块
         self.brightening = 0  # 美白
@@ -93,8 +107,9 @@ class MainWindow(QMainWindow):
         # 执行线程的run方法
         self.camera_thread.start()
 
-    def view_camera(self, img_json):
+    def view_camera(self, img_json, land_mask):
         self.img_json = img_json
+        self.land_mask = land_mask
         # 图像解码
         self.image = self.decoder(self.img_json)
         # 视频每一帧进行处理
@@ -117,17 +132,29 @@ class MainWindow(QMainWindow):
         detector = dlib.get_frontal_face_detector()
         # 获取人脸检测器
         predictor = dlib.shape_predictor(
-            './trainner/shape_predictor_68_face_landmarks.dat'
+            './trainner/models_shape_predictor_81_face_landmarks.dat'
         )
 
         dets = detector(gray, 1)
         for face in dets:
-            # 寻找人脸的68个标定点
+            # 寻找人脸的81个标定点
             shape = predictor(self.image, face)
+            self.land_mask = np.matrix([[p.x, p.y] for p in shape.parts()])
             # 遍历所有点，打印出其坐标，并圈出来
             for pt in shape.parts():
                 pt_pos = (pt.x, pt.y)
                 cv2.circle(self.image, pt_pos, 2, (0, 255, 0), 1)
+
+    def draw_convex_hull(self, mask, points, color):
+        hull = cv2.convexHull(points)
+        cv2.fillConvexPoly(mask, hull, color=color)
+
+    def get_skin_mask(self):
+        img = self.image
+        mask = np.zeros(img.shape[:2])
+        self.draw_convex_hull(mask, self.land_mask[jaw_point], color=1)
+        mask = np.array([mask] * 3).transpose(1, 2, 0)
+        self.skin_mask = mask
 
     def process_dlg(self):
         dlg = ProcessWindow(self.slider_change_signal)
@@ -161,23 +188,61 @@ class MainWindow(QMainWindow):
             self.show_image()
 
     def process_image(self):
+        self.res_image = self.image
+
         if self.brightening != 0:
-            pass
+            self.get_skin_mask()
+            self.face_brightening()
 
         elif self.smooth != 0:
+            self.get_skin_mask()
             pass
 
         elif self.face != 0:
+            self.get_skin_mask()
             pass
 
         elif self.eye != 0:
+            self.get_skin_mask()
             pass
 
         elif self.mouth != 0:
+            self.get_skin_mask()
             pass
 
         elif self.eyebrow != 0:
+            self.get_skin_mask()
             pass
+
+    def face_brightening(self):
+        img = self.image
+        # 设定一个当前图像的副本imgw，并且在美白算法中存储更新后的像素值
+        imgw = np.zeros(img.shape, dtype='uint8')
+        imgw = img.copy()
+        # 设定一个mitones数组，来计算美白算法中的中间调增益，该数组中每个元素都对应一种亮度值
+        midtones_add = np.zeros(256)
+
+        for i in range(256):
+            midtones_add[i] = 0.667 * (1 - ((i - 127.0) / 127) * ((i - 127.0) / 127))
+
+        lookup = np.zeros(256, dtype="uint8")
+
+        for i in range(256):
+            red = i
+            red += np.uint8(self.brightening * midtones_add[red])
+            red = max(0, min(0xff, red))
+            lookup[i] = np.uint8(red)
+
+        rows, cols, channals = img.shape
+        for r in range(rows):
+            for c in range(cols):
+                # 进行皮肤识别 对于图像中被识别为皮肤的像素点 将其RGB值通过查找表进行修改从而达到美白的效果
+                if self.skin_mask[r, c, 0] == 1:
+                    # 如果当前像素点的红色通道值为1 就将RGB三个通道的值通过查找表lookup进行修改
+                    imgw[r, c, 0] = lookup[imgw[r, c, 0]]
+                    imgw[r, c, 1] = lookup[imgw[r, c, 1]]
+                    imgw[r, c, 2] = lookup[imgw[r, c, 2]]
+        self.res_image = imgw
 
     def closeEvent(self, event):
         reply = QMessageBox.question(self, '提示',
